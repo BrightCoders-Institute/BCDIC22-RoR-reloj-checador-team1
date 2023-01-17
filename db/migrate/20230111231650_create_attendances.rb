6cfd27f288ed836e24439f0bb95ccb2620123f4a
alangmuñoz <alangonzalez119@gmail.com>
2023-01-16 18:18:38 -0600

    radio buttons added

--- db/migrate/20230111231650_create_attendances.rb
+++ db/migrate/20230111231650_create_attendances.rb
@@ -2,8 +2,7 @@ class CreateAttendances < ActiveRecord::Migration[7.0]
   def change
     create_table :attendances do |t|
       t.date :date
-      t.timestamp :timeEntrance
-      t.timestamp :timeCheckout
+      t.string :direction
       t.belongs_to :employee
 
       t.timestamps
