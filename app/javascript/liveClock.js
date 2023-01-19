06fc8e55a38331a5d0f2dcd2236a396bf2d2d4ef
AangelF <aflores31@ucol.mx>
2023-01-19 16:05:12 -0600

    fix indentation

--- app/javascript/liveClock.js
+++ app/javascript/liveClock.js
@@ -1,9 +1,9 @@
 const reloj = document.querySelector('.reloj')
 function time() {
-    const date = new Date()
-    let hours = date.getUTCHours();
-    let minutes = date.getUTCMinutes();
-    let seconds = date.getUTCSeconds();
-    reloj.innerHTML = `${("0" + hours).slice(-2)}:${("0" + minutes).slice(-2)}:${("0" + seconds).slice(-2)}`
+	const date = new Date()
+	let hours = date.getUTCHours();
+	let minutes = date.getUTCMinutes();
+	let seconds = date.getUTCSeconds();
+  reloj.innerHTML = `${("0" + hours).slice(-2)}:${("0" + minutes).slice(-2)}:${("0" + seconds).slice(-2)}`
 }
 setInterval(time,1000);
