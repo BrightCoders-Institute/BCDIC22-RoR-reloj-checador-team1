e34031e91a2177dc49a34d9d1f7775b241837aaf
AangelF <aflores31@ucol.mx>
2023-01-25 18:13:17 -0600

    Fix table on db
    Co-authored-by: AlanGonzalezM <alangonzalez119@gmail.com>

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,15 @@
 class AdminsController < ApplicationController
 
-  def index
-    @login_admin = Administrator.first
+  def create
+    @login_admin = Admin.new(loginadmin_params)
+    @login_admin.save ? (redirect_to root_path, success: "login de admin exitoso") : (redirect_to admin_path, danger: "ERROR: no login de admin")
   end
+
+  private
+
+  def loginadmin_params
+    params.require(:admin).permit(:admin, :privateNumber)
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "main_pages#index", as: 'main_pages'
+  root "main_pages#index"
   resources :attendances
 
-  get '/admins/index', to: 'admins#index', as: 'login_admin'
-  resources :login_admins
+  get '/admins/index', to: 'admins#index', as: 'admin'
 
 end
